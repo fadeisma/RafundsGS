552f1d8868d161d81c8e498750e601c5a03c7d2a
Fadi Ismael <fadi.ismael@ironsrc.com>
2018-07-06 12:48:03 +0300

    validate all input is as required

--- modules/Calc.js
+++ modules/Calc.js
@@ -5,17 +5,23 @@ var passengersCost = require('../staitcInfo/PasengersConst')
 let trasnformationDetails = {}
 
 exports.getRequest = (data) =>{
+    // in case car was choosed from client side.
     if (data.transportationType === 'Car'){
-        var element = tranportationcCarArea.TranportationCarArea.find((area) =>{ 
+        var areaCost = tranportationcCarArea.TranportationCarArea.find((area) =>{ 
             return area.transportationArea ===  data.area;
         });
-      
-        var currentPassenger = passengersCost.PassengersCost.find((passCost) =>{ 
+        var passCostclient = passengersCost.PassengersCost.find((passCost) =>{ 
             return passCost.id ==  data.passengersNumber;
         });
+        // 
+        console.log('date =' , data.date );
+        console.log('data.transportationType =' , data.transportationType );
+        console.log('areaCost =' , areaCost.cost );
+        console.log('passengersCost =' , passCostclient.cost );
+        console.log('AddtionalCost =' , data.addtionalCost );
+        console.log(Number(passCostclient.cost)+ Number(data.addtionalCost)  + Number(areaCost.cost))
 
     }
-   // console.log('tranportationcCarArea' ,tranportationcCarArea.TranportationCarArea.TelAviv);
-   // console.log(data.trasnporationDetails.Trran)
+    
  
 }
